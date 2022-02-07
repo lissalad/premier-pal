f61204ef0a6d308fd2359ed379f21bc2587da959
lissalad <elissalaymon@gmail.com>
2022-02-07 12:24:02 -0800

    ready to set up db and api

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+host = os.environ.get("DB_URL")
 client = MongoClient()
-db = client.Premiere-PAL
+db = client.Premiere.PAL
 # users = db.users
 collections = db.collections
 movies = db.movies
+
 
 @app.route('/')
 def home():
@@ -16,6 +19,11 @@ def home():
 @app.route('/collections')
 def collections():
   return render_template('collections.html')
+
+
+@app.route('/user')
+def user():
+  return render_template('user.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
