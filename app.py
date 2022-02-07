6428bc659316b0c941ffe7418a25120ea0a250b9
lissalad <elissalaymon@gmail.com>
2022-02-06 16:41:00 -0800

    bones

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-# lissa: I love forks
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/collections')
+def collections():
+  return render_template('collections.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
 
 print("lissa says she climbs development trees")
