eb3d4b78147d0e3dd97ecdf8582ca45f32531ac0
Ahyeonn <ahyeon971030@gmail.com>
2022-02-14 11:28:32 -0800

    Added Overview Route

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ db = client.Premiere.PAL
 collections = db.collections
 movies = db.movies
 
-
 @app.route('/')
 def home():
   return render_template('index.html')
@@ -20,6 +19,11 @@ def home():
 def collections():
   return render_template('collections.html')
 
+movie = {'title': 'Frozen', 'overview': 'abcd', 'release_date': 'November 27, 2013', 'poster_path': '/1eQ3c443YwXz1Xq0FZ24qrJBKyd.jpg', 'genre_ids': '[53, 80]'}
+
+@app.route('/overview')
+def overview():
+  return render_template('overview.html', movie=movie)
 
 @app.route('/user')
 def user():
