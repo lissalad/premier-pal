65283c779e33252c18e0141c7a242bf5fb8692a7
Ahyeonn <ahyeon971030@gmail.com>
2022-02-16 10:59:59 -0800

    Added Collection Route

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 host = os.environ.get("DB_URL")
 client = MongoClient()
-db = client.Premiere.PAL
+db = client.Premiere_PAL
 # users = db.users
-collections = db.collections
+movie_colls = db.movie_coll
 movies = db.movies
 
 @app.route('/')
@@ -17,8 +17,22 @@ def home():
 
 @app.route('/collections')
 def collections():
-  return render_template('collections.html')
+  return render_template('collections.html', collections=movie_colls.find())
 
+@app.route('/collections/new')
+def collections_new():
+  return render_template('new-collection.html', collection ={})
+
+@app.route('/collections', methods=['POST'])
+def collections_submit():
+  collection = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description')
+  }
+  movie_colls.insert_one(collection)
+  return redirect(url_for('collections'))
+
+# Overview
 movie = {'title': 'Frozen', 'overview': 'abcd', 'release_date': 'November 27, 2013', 'poster_path': '/1eQ3c443YwXz1Xq0FZ24qrJBKyd.jpg', 'genre_ids': '[53, 80]'}
 
 @app.route('/overview')
